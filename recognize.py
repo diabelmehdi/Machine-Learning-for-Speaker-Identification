e880793e068941e3eab8288588d5222ce52e64a7
unknown <diab.elmehd@gmail.com>
2020-08-06 17:09:51 +0000

    main files

---
+++ recognize.py
@@ -0,0 +1,94 @@
+import pyaudio
+import wave
+import os
+import pickle
+import time
+from scipy.io.wavfile import read
+from IPython.display import Audio, display, clear_output
+
+from main_functions import *
+
+def recognize():
+    # Voice Authentication
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
+    CHUNK = 1024
+    RECORD_SECONDS = 4
+    FILENAME = "./test.wav"
+
+    for i in range(1):
+        audio = pyaudio.PyAudio()
+        if i == 0:
+            j = 3
+            while j>=0:
+                time.sleep(1.0)
+                print("Speak your name in {} seconds".format(j))
+                clear_output(wait=True)
+
+                j-=1   
+    # start Recording
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("recording...")
+    frames = []
+
+    for _ in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    print("finished recording")
+
+
+    # stop Recording
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    # saving wav file 
+    waveFile = wave.open(FILENAME, 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(frames))
+    waveFile.close()
+
+    modelpath = "./gmm_models/"
+
+    gmm_files = [os.path.join(modelpath,fname) for fname in 
+                os.listdir(modelpath) if fname.endswith('.gmm')]
+
+    models    = [pickle.load(open(fname,'rb')) for fname in gmm_files]
+
+    speakers   = [fname.split("/")[-1].split(".gmm")[0] for fname 
+                in gmm_files]
+  
+    if len(models) == 0:
+        print("No Users in the Database!")
+        return
+        
+    #read test file
+    sr,audio = read(FILENAME)
+
+    # extract mfcc features
+    vector = extract_features(audio,sr)
+    log_likelihood = np.zeros(len(models)) 
+
+    #checking with each model one by one
+    for i in range(len(models)):
+        gmm = models[i]         
+        scores = np.array(gmm.score(vector))
+        log_likelihood[i] = scores.sum()
+
+    pred = np.argmax(log_likelihood)
+    identity = speakers[pred]
+   
+    # if voice not recognized than terminate the process
+    if identity == 'unknown':
+            print("Not Recognized! Try again...")
+            return
+    
+    print( "Recognized as - ", identity)
+if __name__ == '__main__':
+    recognize()
